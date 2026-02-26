0a14b293561a28fdaa3ca626a6f68eb7bc082120
Niels Clarysse <niels.clarysse@student.howest.be>
2026-02-26 18:54:07 +0100

    Completes ex5

--- app/src/main/java/be/howest/annaudenaert/sweetdroiddelights/ui/navigation/SweetDroidAppBar.kt
+++ app/src/main/java/be/howest/annaudenaert/sweetdroiddelights/ui/navigation/SweetDroidAppBar.kt
@@ -1,7 +1,13 @@
 package be.howest.annaudenaert.sweetdroiddelights.ui.navigation
 
 import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -9,22 +15,27 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import be.howest.annaudenaert.sweetdroiddelights.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SweetDroidAppBar(
-    modifier: Modifier = Modifier.Companion,
-    @StringRes appName: Int
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit
 ) {
     TopAppBar(
-        modifier = modifier,
-        title = {
-            Text(stringResource(appName))
+        title = { Text(stringResource(id = R.string.app_name)) },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
         },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            titleContentColor = MaterialTheme.colorScheme.onPrimary,
-            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
-        )
+        actions = {
+            IconButton(onClick = shareWithFriends) {
+                Icon(Icons.Filled.Share, contentDescription = null)
+            }
+        }
     )
 }
