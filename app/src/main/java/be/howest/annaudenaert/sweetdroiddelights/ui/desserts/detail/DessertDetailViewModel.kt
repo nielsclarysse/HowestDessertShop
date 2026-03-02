04fe1ca393e56c7f935dbf49bcdab05bf3bdb453
Niels Clarysse <niels.clarysse@student.howest.be>
2026-03-02 19:45:00 +0100

    Completes ex9

--- app/src/main/java/be/howest/annaudenaert/sweetdroiddelights/ui/desserts/detail/DessertDetailViewModel.kt
+++ app/src/main/java/be/howest/annaudenaert/sweetdroiddelights/ui/desserts/detail/DessertDetailViewModel.kt
@@ -1,19 +1,27 @@
 package be.howest.annaudenaert.sweetdroiddelights.ui.desserts.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import be.howest.annaudenaert.sweetdroiddelights.data.fake.FakeDataSource
 import be.howest.annaudenaert.sweetdroiddelights.domain.model.Dessert
+import be.howest.annaudenaert.sweetdroiddelights.domain.usecases.AddDessertToCartUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class DessertDetailUiState(
     val dessert: Dessert? = null,
     val error: String? = null
 )
 
-class DessertDetailViewModel : ViewModel() {
+@HiltViewModel
+class DessertDetailViewModel @Inject constructor(
+    private val addDessertToCart: AddDessertToCartUseCase
+) : ViewModel() {
     private val _uiState = MutableStateFlow(DessertDetailUiState())
     val uiState: StateFlow<DessertDetailUiState> = _uiState
 
@@ -23,6 +31,14 @@ class DessertDetailViewModel : ViewModel() {
             _uiState.update { it.copy(dessert = result) }
         } catch (e: Exception) {
             _uiState.update { it.copy(error = "Dessert could not be loaded") }
+        }
+    }
+
+    fun onAddToCartClicked() {
+        viewModelScope.launch {
+            uiState.value.dessert?.let {
+                addDessertToCart(it)
+            }
         }
     }
 }
